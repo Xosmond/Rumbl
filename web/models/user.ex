8179e14630f22644d3e11a228ce06d172366a56f
Jordano Moscoso <jor_notes@outlook.com>
2017-06-26 21:55:54 -0500

    Can create new users.

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,13 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
 
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username, :password])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 3, max: 20)
+  end
+
   schema "users" do
     field :name, :string
     field :username, :string
