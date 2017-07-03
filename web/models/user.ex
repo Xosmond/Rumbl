76d446e4e90e23c04228423a03eb3cfb32b221bb
Jordano Moscoso <jor_notes@outlook.com>
2017-07-03 09:05:56 -0500

    Added comeonin, and changeset for secure password

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,11 +1,29 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
 
-  def changeset(model, params \\ %{}) do
+  def changeset(model, params \\ :empty) do
     model
-    |> cast(params, [:name, :username, :password])
-    |> validate_required([:name, :username])
-    |> validate_length(:username, min: 3, max: 20)
+    |> cast(params, [:name, :username, :password]) # Allowed values
+    |> validate_required([:name, :username]) # Required values
+    |> validate_length(:username, min: 3, max: 20) # Required values
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  def put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset {valid?: true, changes: %{password: password }} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        changeset
+    end
   end
 
   schema "users" do
